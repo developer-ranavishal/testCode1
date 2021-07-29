47c72d990727840dd9cd8e8146d75b431236b9e0
Vishal Rana <vishalrana9038@gmail.com>
2021-07-29 08:39:32 +0530

    line 2 added in demo1

--- app/src/main/java/com/example/testcode/MainActivity2.kt
+++ app/src/main/java/com/example/testcode/MainActivity2.kt
@@ -33,7 +33,7 @@ class MainActivity2 : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-     //hey you are good enough
+     //hey this is line 2 in demo1
 
     }
 
